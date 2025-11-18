79bb53db1c603fc1aaf221a2e1064acb3be02986
ZakharDay <zakhar.day@gmail.com>
2025-11-18 05:42:04 +0300

    Config fixes

--- bun.config.prod.js
+++ bun.config.prod.js
@@ -1,15 +1,6 @@
-// await Bun.build({
-//   entrypoints: ['./src/index.html'],
-//   outdir: './dist',
-//   minify: true,
-//   splitting: false,
-//   // sourcemap: 'external',
-//   target: 'browser' // or 'bun', 'node', 'browser'
-// }).catch(console.error)
-
 await Bun.build({
   entrypoints: ['./src/index.html'],
   outdir: './dist',
   minify: true,
-  target: 'browser' // default
+  target: 'browser'
 })
